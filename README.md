a7b3e270ed8b2ea11c432aa9181083289d7e37a9
SandyL88 <SANDY66888866@OUTLOOK.COM>
2024-05-22 09:47:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Visual-studio-basic-webpage
+ using visual studio to creat basic website with video
---
+++ WebApplication1/WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// JavaScript
+// JavaScript
+function loadText() {
+    const paragraph = document.getElementById('video_discription');
+    const text = paragraph.innerText;
+    paragraph.innerText = '';
+
+
+    let index = 0;
+    const interval = 100;
+    const timer = setInterval(function () {
+        paragraph.textContent += text[index++];
+        if (index >= text.length) {
+            clearInterval(timer);
+        }
+    }, interval);
+};
+document.addEventListener('DOMContentLoaded', loadText);
